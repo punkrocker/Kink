088d0c0d39f2f00017e088fd1baa8efca64af292
punkrocker <punkrock_2000@163.com>
2019-08-11 20:59:18 +0800

    重构:修改变量命名规则

--- GetKinkByChannel.py
+++ GetKinkByChannel.py
@@ -7,15 +7,15 @@ import Config
 import sys
 
 
-def getChannel(channelName, endPage):
+def get_channel(channel_name, end_page):
     user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
     headers = {'User-Agent': user_agent, 'Cookie': 'viewing-preferences=straight'}
-    for page in range(1, int(endPage) + 1):
-        url = "https://www.kink.com/channel/" + channelName + "/latest/page/" + str(page)
-        getUrlContent(headers, url, channelName)
+    for page in range(1, int(end_page) + 1):
+        url = "https://www.kink.com/channel/" + channel_name + "/latest/page/" + str(page)
+        get_url_content(headers, url, channel_name)
 
 
-def getUrlContent(headers, url, channelName):
+def get_url_content(headers, url, channel_name):
     try:
         req = urllib.request.Request(url, None, headers)
         all_the_text = urlopen(req).read()
@@ -28,16 +28,16 @@ def getUrlContent(headers, url, channelName):
         for child in shoots:
             try:
                 performer = child.find("div", {"class", "shoot-thumb-models"}).get_text().strip()
-                videoName = child.find("div", {"class", "script"}).get_text().strip()
-                videoNo = child.a.get("href").replace("/shoot/", "")
+                video_name = child.find("div", {"class", "script"}).get_text().strip()
+                video_no = child.a.get("href").replace("/shoot/", "")
                 pic = child.find("img", {"class", "adimage"}).get("src")
                 if (not pic.startswith("http")):
-                    continue;
+                    continue
                 try:
                     conn = pymysql.connect(host=Config.HostIP, user=Config.UserName, passwd=Config.Password,
                                            db=Config.DBName, port=Config.Port, charset=Config.Charset)
                     cur = conn.cursor()
-                    sql = "select count(1) as num from t_videos where videono = '" + videoNo + "'"
+                    sql = "select count(1) as num from t_videos where videono = '" + video_no + "'"
                     cur.execute(sql)
                     r = cur.fetchall()
                     if (r[0][0] > 0):
@@ -46,20 +46,20 @@ def getUrlContent(headers, url, channelName):
                         # 下载图片
                         try:
                             path = Config.Path
-                            pic_location = path + videoNo + ".jpg"
+                            pic_location = path + video_no + ".jpg"
                             picconn = urllib.request.urlopen(pic)
                             f = open(pic_location, 'wb')
                             f.write(picconn.read())
                             f.close()
                         except HTTPError as e:
                             print(e)
-                    sql = "insert into t_videos (companyname,videono,status,performer,img,videoname,channel) values ('Kink','" + videoNo.replace(
+                    sql = "insert into t_videos (companyname,videono,status,performer,img,videoname,channel) values ('Kink','" + video_no.replace(
                         '\'', '\\\'') + "','0','" + performer.replace('\'',
-                                                                      '\\\'') + "','" + videoNo + ".jpg" + "',' " + videoName.replace(
-                        '\'', '\\\'') + " ','" + channelName + "')"
+                                                                      '\\\'') + "','" + video_no + ".jpg" + "',' " + video_name.replace(
+                        '\'', '\\\'') + " ','" + channel_name + "')"
                     cur.execute(sql)
                     conn.commit()
-                    print(videoNo + ":SUCCESS")
+                    print(video_no + ":SUCCESS")
                 except Exception as ex:
                     print(ex)
                 finally:
@@ -83,5 +83,5 @@ else:
     result = soup.find("div", {"class", "shoot-list"})
     shoots = result.findAll("a", {"class", "page-normal"})
     last = shoots[-1].get_text().strip()
-    getChannel(sys.argv[1], last)
+    get_channel(sys.argv[1], last)
 print("finish")
