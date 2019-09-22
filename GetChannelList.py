bd1840125443be0171f1e782e5d725a897b1ad00
punkrocker <punkrock_2000@163.com>
2019-09-22 23:43:37 +0800

    提取解析文本功能

--- GetChannelList.py
+++ GetChannelList.py
@@ -24,51 +24,55 @@ def get_url_content(url, channel_name):
     except HTTPError as e:
         print(e)
     else:
-        soup = BeautifulSoup(all_the_text, "html.parser")
-        result = soup.find("div", {"class", "shoot-list"})
-        shoots = result.findAll("div", {"class", "shoot"})
-        for child in shoots:
+        parse_html(all_the_text, channel_name)
+
+
+def parse_html(all_the_text, channel_name):
+    soup = BeautifulSoup(all_the_text, "html.parser")
+    result = soup.find("div", {"class", "shoot-list"})
+    shoots = result.findAll("div", {"class", "shoot"})
+    for child in shoots:
+        try:
+            performer = child.find("div", {"class", "shoot-thumb-models"}).get_text().strip()
+            video_name = child.find("div", {"class", "script"}).get_text().strip()
+            video_no = child.a.get("href").replace("/shoot/", "")
+            pic = child.find("img", {"class", "adimage"}).get("src")
+            if (not pic.startswith("http")):
+                continue
             try:
-                performer = child.find("div", {"class", "shoot-thumb-models"}).get_text().strip()
-                video_name = child.find("div", {"class", "script"}).get_text().strip()
-                video_no = child.a.get("href").replace("/shoot/", "")
-                pic = child.find("img", {"class", "adimage"}).get("src")
-                if (not pic.startswith("http")):
+                conn = pymysql.connect(host=Config.HostIP, user=Config.UserName, passwd=Config.Password,
+                                       db=Config.DBName, port=Config.Port, charset=Config.Charset)
+                cur = conn.cursor()
+                sql = "select count(1) as num from t_videos where videono = '" + video_no + "'"
+                cur.execute(sql)
+                r = cur.fetchall()
+                if (r[0][0] > 0):
                     continue
-                try:
-                    conn = pymysql.connect(host=Config.HostIP, user=Config.UserName, passwd=Config.Password,
-                                           db=Config.DBName, port=Config.Port, charset=Config.Charset)
-                    cur = conn.cursor()
-                    sql = "select count(1) as num from t_videos where videono = '" + video_no + "'"
-                    cur.execute(sql)
-                    r = cur.fetchall()
-                    if (r[0][0] > 0):
-                        continue
-                    else:
-                        # 下载图片
-                        try:
-                            path = Config.Path
-                            pic_location = path + video_no + ".jpg"
-                            picconn = urllib.request.urlopen(pic)
-                            f = open(pic_location, 'wb')
-                            f.write(picconn.read())
-                            f.close()
-                        except HTTPError as e:
-                            print(e)
-                    sql = "insert into t_videos (companyname,videono,status,performer,img,videoname,channel) values ('Kink','" + video_no.replace(
-                        '\'', '\\\'') + "','0','" + performer.replace('\'',
-                                                                      '\\\'') + "','" + video_no + ".jpg" + "',' " + video_name.replace(
-                        '\'', '\\\'') + " ','" + channel_name + "')"
-                    cur.execute(sql)
-                    conn.commit()
-                    print(video_no + ":SUCCESS")
-                except Exception as ex:
-                    print(ex)
-                finally:
-                    cur.close()
-                    conn.close()
-            except Exception as e:
-                print(e)
+                else:
+                    # 下载图片
+                    try:
+                        path = Config.Path
+                        pic_location = path + video_no + ".jpg"
+                        picconn = urllib.request.urlopen(pic)
+                        f = open(pic_location, 'wb')
+                        f.write(picconn.read())
+                        f.close()
+                    except HTTPError as e:
+                        print(e)
+                sql = "insert into t_videos (companyname,videono,status,performer,img,videoname,channel) values ('Kink','" + video_no.replace(
+                    '\'', '\\\'') + "','0','" + performer.replace('\'',
+                                                                  '\\\'') + "','" + video_no + ".jpg" + "',' " + video_name.replace(
+                    '\'', '\\\'') + " ','" + channel_name + "')"
+                cur.execute(sql)
+                conn.commit()
+                print(video_no + ":SUCCESS")
+            except Exception as ex:
+                print(ex)
+            finally:
+                cur.close()
+                conn.close()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
