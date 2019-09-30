1bd5a06bd214f74ede2b9872304fcc1ba1700c72
punkrocker <punkrock_2000@163.com>
2019-09-30 22:48:16 +0800

    修改数据库链接地址

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
-HostIP = 'localhost'
+HostIP = '10.0.0.138'
 UserName = 'root'
-Password = '123'
-DBName = 'magnet'
+Password = '123123'
+DBName = 'kink'
 Port = 3306
 Charset = 'utf8'
 Path = "E:\\Project\\MagnetShow\\Public\\imgs\\downloaded\\"
