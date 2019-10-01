a935e5740772cf96f510984710541ac854ad56bb
punkrocker <punkrock_2000@163.com>
2019-10-01 23:02:12 +0800

    提取通用配置

--- Config.py
+++ Config.py
@@ -5,3 +5,6 @@ DBName = 'kink'
 Port = 3306
 Charset = 'utf8'
 Path = "E:\\Project\\MagnetShow\\Public\\imgs\\downloaded\\"
+
+user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
+headers = {'User-Agent': user_agent, 'Cookie': 'viewing-preferences=straight'}
