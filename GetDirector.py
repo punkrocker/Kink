6a763925897e6a3a9f2e17125f003402491cbd63
punkrocker <punkrock_2000@163.com>
2019-10-03 01:24:55 +0900

    使用Career类读取导演页面

--- GetDirector.py
+++ GetDirector.py
@@ -1,19 +1,7 @@
-from bs4 import BeautifulSoup
-
-
-def get_model_name(soup):
-    model_name = soup.find('h1', {'class', 'page-title'})
-    [s.extract() for s in model_name('span')]
-    return str(model_name.text).strip()
-
-
-def parse_html(all_the_text):
-    soup = BeautifulSoup(all_the_text, 'html.parser')
-    model_name = get_model_name(soup)
-    desc = str(soup.find('span', id='expand-text').text).strip()
-    print(model_name, desc)
-
+from Career import Career
 
 if __name__ == '__main__':
     content = open('./target_html/director.html', 'r', encoding='utf-8').read()
-    parse_html(content)
+    director = Career(content, is_model=False)
+    print(director.name)
+    print(director.desc)
