3c073a5b85c0201e1dcd994d314c838b7bcb4bdc
punkrocker <punkrock_2000@163.com>
2019-10-03 01:23:09 +0900

    使用Career类读取模特页面

--- GetModel.py
+++ GetModel.py
@@ -1,20 +1,8 @@
-from bs4 import BeautifulSoup
-
-
-def get_model_name(soup):
-    model_name = soup.find('h1', {'class', 'page-title'})
-    [s.extract() for s in model_name('span')]
-    return str(model_name.text).strip()
-
-
-def parse_html(all_the_text):
-    soup = BeautifulSoup(all_the_text, 'html.parser')
-    model_name = get_model_name(soup)
-    desc = str(soup.find('span', id='expand-text').text).strip()
-    tags = str(soup.find('div', {'class', 'model-tags'}).text).strip()
-    tags = tags.replace('tags:', '').replace('\n', '').strip()
-
+from Career import Career
 
 if __name__ == '__main__':
     content = open('./target_html/model.html', 'r', encoding='utf-8').read()
-    parse_html(content)
+    model = Career(content)
+    print(model.name)
+    print(model.desc)
+    print(model.tags)
