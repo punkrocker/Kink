796dd9e0a44b7bd38dcdbc38b6429406977866c4
punkrocker <punkrock_2000@163.com>
2019-10-04 23:49:12 +0800

    获取video编号及系列名

--- GetVideo.py
+++ GetVideo.py
@@ -10,10 +10,15 @@ def get_shoot_name(soup):
 def parse_html(all_the_text):
     soup = BeautifulSoup(all_the_text, 'html.parser')
     shoot_title = get_shoot_name(soup)
+    shoot_base = soup.find('div', {'class', 'shoot-page'})
+    shoot_id = shoot_base['data-shootid']
+    shoot_channel = shoot_base['data-sitename']
     # desc = str(soup.find('span', id='expand-text').text).strip()
     # tags = str(soup.find('div', {'class', 'model-tags'}).text).strip()
     # tags = tags.replace('tags:', '').replace('\n', '').strip()
     print(shoot_title)
+    print(shoot_channel)
+    print(shoot_id)
 
 
 if __name__ == '__main__':
