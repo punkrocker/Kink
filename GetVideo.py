1ccf827723ba25aa930733b4dd0318dde5e2c0c6
punkrocker <punkrock_2000@163.com>
2019-10-07 16:57:35 +0800

    使用shoot类解析视频页面

--- GetVideo.py
+++ GetVideo.py
@@ -1,44 +1,13 @@
-from bs4 import BeautifulSoup
-import datetime
-
-
-def get_shoot_name(soup):
-    model_name = soup.find('h1', {'class', 'shoot-title'})
-    [s.extract() for s in model_name('span')]
-    return str(model_name.text).strip()
-
-
-def parse_html(all_the_text):
-    soup = BeautifulSoup(all_the_text, 'html.parser')
-    shoot_title = get_shoot_name(soup)
-    shoot_base = soup.find('div', {'class', 'shoot-page'})
-    shoot_id = shoot_base['data-shootid']
-    shoot_channel = shoot_base['data-sitename']
-    performers = soup.find('p', {'class', 'starring'})
-    performers = performers.findAll('a')
-    # for p in performers:
-    #     print(p.text)
-    # directors = soup.find('p', {'class', 'director'})
-    # directors = directors.findAll('a')
-    # for d in directors:
-    #     print(d.text)
-    # tags = soup.find('p', {'class', 'category-tag-list'})
-    # tags = tags.findAll('a')
-    # for t in tags:
-    #     print(t.text)
-    desc = str(soup.find('div', {'class', 'description'}).text).strip()
-    date = soup.find('div', {'class', 'shoot-info'})
-    date = date.find('div', {'class', 'columns'})
-    date = date.find('div', {'class', 'column'})
-    date = str(date.find('p').text).strip().replace('Date: ', '')
-    time_format = datetime.datetime.strptime(date, '%B %d, %Y')  # datetime.datetime(2016, 11, 18, 0, 0)
-    upload_date = datetime.datetime.strftime(time_format, '%Y-%m-%d')
-    download_content = soup.find('div', {'class', 'member-content'})
-    download_content = download_content.find('div', {'class', 'full'})
-    download_link = download_content.find('li').find('a')
-    print(download_link['href'])
-
+from dto.Shoot import Shoot
 
 if __name__ == '__main__':
     content = open('./target_html/video.html', 'r', encoding='utf-8').read()
-    parse_html(content)
+    shoot = Shoot(content)
+    print(shoot.shoot_id)
+    print(shoot.shoot_name)
+    print(shoot.channel_name)
+    print(shoot.actor_names)
+    print(shoot.director_names)
+    print(shoot.categories)
+    print(shoot.description)
+    print(shoot.download_link)
--- dto/Shoot.py
+++ dto/Shoot.py
@@ -1,19 +1,50 @@
 import datetime
+from bs4 import BeautifulSoup
 
 
 class Shoot:
-    def __init__(self, soup):
+    def __init__(self, text):
         self.shoot_id = ''
         self.shoot_name = ''
         self.actor_names = []
-        self.director_name = ''
+        self.director_names = []
         self.categories = []
         self.description = ''
         self.shoot_date = datetime.date
         self.channel_name = ''
         self.download_link = ''
+        self.parse_html(text)
 
     def get_shoot_name(self, soup):
         model_name = soup.find('h1', {'class', 'shoot-title'})
         [s.extract() for s in model_name('span')]
         self.shoot_name = str(model_name.text).strip()
+
+    def parse_html(self, all_the_text):
+        soup = BeautifulSoup(all_the_text, 'html.parser')
+        self.get_shoot_name(soup)
+        shoot_base = soup.find('div', {'class', 'shoot-page'})
+        self.shoot_id = shoot_base['data-shootid']
+        self.channel_name = shoot_base['data-sitename']
+        performers = soup.find('p', {'class', 'starring'})
+        performers = performers.findAll('a')
+        for p in performers:
+            self.actor_names.append(p.text)
+        directors = soup.find('p', {'class', 'director'})
+        directors = directors.findAll('a')
+        for d in directors:
+            self.director_names.append(d.text)
+        tags = soup.find('p', {'class', 'category-tag-list'})
+        tags = tags.findAll('a')
+        for t in tags:
+            self.categories.append(t.text)
+        self.description = str(soup.find('div', {'class', 'description'}).text).strip()
+        date = soup.find('div', {'class', 'shoot-info'})
+        date = date.find('div', {'class', 'columns'})
+        date = date.find('div', {'class', 'column'})
+        date = str(date.find('p').text).strip().replace('Date: ', '')
+        time_format = datetime.datetime.strptime(date, '%B %d, %Y')
+        self.shoot_date = datetime.datetime.strftime(time_format, '%Y-%m-%d')
+        download_content = soup.find('div', {'class', 'member-content'})
+        download_content = download_content.find('div', {'class', 'full'})
+        self.download_link = download_content.find('li').find('a')['href']
