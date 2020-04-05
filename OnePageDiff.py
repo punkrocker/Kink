368096ac6e9f5be692ddb7c8a118f910a081c2a6
punkrocker <punkrock_2000@163.com>
2020-04-06 02:21:22 +0800

    对比单页缺少视频

--- OnePageDiff.py
+++ OnePageDiff.py
@@ -3,11 +3,12 @@
 """
 
 from bs4 import BeautifulSoup
-import sys
+import os
+
+video_path = "G:\\"
 
 user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
 headers = {'User-Agent': user_agent, 'Cookie': 'viewing-preferences=straight'}
-
 content = open('./target_html/video_list_page.html', 'r', encoding='utf-8').read()
 soup = BeautifulSoup(content, 'html.parser')
 result = soup.find("div", {"class", "shoot-list"})
@@ -19,4 +20,20 @@ for child in shoots:
         page_videos.append(video_no)
     except Exception as e:
         print(e)
-print(page_videos)
+
+files = os.listdir(video_path)
+video_files = []
+for file in files:
+    if file.endswith('.mp4'):
+        video_files.append(file)
+
+downloaded_video = []
+for number in page_videos:
+    for file in video_files:
+        if number in file:
+            downloaded_video.append(number)
+            continue
+
+x = set(page_videos) - set(downloaded_video)
+print(x)
+print(len(x))
