70ee4617012603edf2f1371a8e2a8deaa781b547
punkrocker <punkrock_2000@163.com>
2020-05-31 20:04:33 +0800

    支持多文件夹对比

--- OnePageDiff.py
+++ OnePageDiff.py
@@ -5,35 +5,50 @@
 from bs4 import BeautifulSoup
 import os
 
-video_path = "G:\\"
+video_path = ["O:\\boundgangbangs\\2020", "O:\\boundgangbangs\\2019"]
 
-user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
-headers = {'User-Agent': user_agent, 'Cookie': 'viewing-preferences=straight'}
-content = open('./target_html/video_list_page.html', 'r', encoding='utf-8').read()
-soup = BeautifulSoup(content, 'html.parser')
-result = soup.find("div", {"class", "shoot-list"})
-shoots = result.findAll("div", {"class", "shoot"})
-page_videos = []
-for child in shoots:
-    try:
-        video_no = child.a.get("href").replace("/shoot/", "")
-        page_videos.append(video_no)
-    except Exception as e:
-        print(e)
 
-files = os.listdir(video_path)
-video_files = []
+def get_diff(paths):
+    user_agent = 'Mozilla/5.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = {'User-Agent': user_agent, 'Cookie': 'viewing-preferences=straight'}
+    content = open('./target_html/video_list_page.html', 'r', encoding='utf-8').read()
+    soup = BeautifulSoup(content, 'html.parser')
+    result = soup.find("div", {"class", "shoot-list"})
+    shoots = result.findAll("div", {"class", "shoot"})
+    page_videos = []
+    for child in shoots:
+        try:
+            video_no = child.a.get("href").replace("/shoot/", "")
+            page_videos.append(video_no)
+        except Exception as e:
+            print(e)
+    video_files = []
+    for path in paths:
+        files = os.listdir(path)
+        for file in files:
+            if file.endswith('.mp4'):
+                video_files.append(file)
+    downloaded_video = []
+    for number in page_videos:
+        for file in video_files:
+            if number in file:
+                downloaded_video.append(number)
+                continue
+    return page_videos, downloaded_video
+
+
+page_videos, downloaded_videos = get_diff(video_path)
+
+not_in_path = set(page_videos) - set(downloaded_videos)
+for a in not_in_path:
+    print(a)
+print(len(not_in_path))
+
+print('=========================================')
+# downloaded_path = 'P:\\Tuf\\转移tUF'
+downloaded_path = 'Q:\\转移'
+files = os.listdir(downloaded_path)
 for file in files:
-    if file.endswith('.mp4'):
-        video_files.append(file)
-
-downloaded_video = []
-for number in page_videos:
-    for file in video_files:
-        if number in file:
-            downloaded_video.append(number)
-            continue
-
-x = set(page_videos) - set(downloaded_video)
-print(x)
-print(len(x))
+    for num in not_in_path:
+        if (num in file):
+            print(file)
